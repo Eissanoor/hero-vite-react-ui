d024f8bdc91a3a3c2def98c250812f77ee4330e5
Eissanoor <eissaanoor@gmail.com>
2026-01-05 16:38:11 +0500

    awdasd

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 // API configuration
 const API_CONFIG = {
-  // BASE_URL: 'https://shahid-tan.vercel.app',
-  BASE_URL: 'http://localhost:5000',
+  BASE_URL: 'https://shahid-tan.vercel.app',
+  // BASE_URL: 'http://localhost:5000',
   ENDPOINTS: {
     LOGIN: '/api/users/login',
     ME: '/api/users/me',
